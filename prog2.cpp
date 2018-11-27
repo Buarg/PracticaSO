22cd476eba2cc007a9c2ed804908a02a190a7aad
Buarg <hector_buarg@hotmail.com>
2018-11-27 13:57:27 +0100

    WIP

--- prog2.cpp
+++ prog2.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<cstring>
+#include<fstream>
+#include<cstdlib>
 
 using namespace std;
 
@@ -7,21 +9,47 @@ void trocear_linea(char linea[], const char delim[], const int maxElems, char *l
 
 int main(int argc, char *argv[])
 {
-	char c1[256];
+	char c1[256] = "";
 	const char DELIMITADOR[] = " \t";
 	const int MAX_ELEMENTOS = 21;
 	char *trozos[MAX_ELEMENTOS + 1];
 
 	switch(argc)
 	{
-		case 1: cout << "Introduce una cadena: " << endl;
-			cin.getline(c1, 256);
-			cin.clear();
-			
-		case 2: 
+		case 1: while(strcmp(c1, "salir") != 0) {	
+				cout << "Introduce una cadena: " << endl;
+				cin.getline(c1, 256);
+				cin.clear();
+
+				trocear_linea(c1, DELIMITADOR, MAX_ELEMENTOS, trozos);
+
+				for(int i = 0; trozos[i] != NULL; i++) {
+				cout << trozos[i] << endl;
+				}
+			}
+			break;
+
+		case 2: ifstream e;
+			e.open(argv[1]);
+			if(e.fail()) {
+				cerr << "ERROR: No se puede abrir el archivo" << argv[1] << endl;
+				exit(2);
+			}
+			else {
+				e.getline(c1, 255, '\n');
+				while(! e.eof()) {
+					trocear_linea(c1, DELIMITADOR, MAX_ELEMENTOS, trozos);
+					e.getline(c1, 255, '\n');
+				}
+				e.close();
+			}
+			break;
 
 		default: cerr << "ERROR: Demasiados argumentos." << endl;
+			 break;
 	}
+
+	exit(0);
 }
 
 void trocear_linea(char linea[], const char delim[], const int maxElems, char *lista_elementos[])
